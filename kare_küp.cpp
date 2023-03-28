5532cce0531b9f2fec46d382b5e899dcab91785f
omertuna46 <128528564+omertuna46@users.noreply.github.com>
2023-03-28 20:57:01 +0300

    update_2023.03.28_1

---
+++ kare_küp.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+ using namespace std;
+
+ int main()
+
+ {
+  int sayi, kare, kup;
+
+  cout << "Bir Sayi Giriniz:" <<sayi;
+
+  cin>>sayi;
+
+
+  kare= sayi * sayi;
+  kup= sayi * sayi * sayi;
+   cout<<"Girdiginiz Sayi:" <<sayi<<endl;
+
+   cout<<"karesi:" <<kare<<endl;
+
+  cout<<"kupu:" <<kup<<endl;
+
+
+
+   return 0;
+ }
---
+++ sayisayi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+ using namespace std;
+
+ int main()
+
+ {
+  int sayi;
+
+  cout << "Bir Sayi Giriniz:" <<sayi;
+
+  cin>>sayi;
+
+   cout<<"Girdiginiz Sayi:" <<sayi<<endl;
+
+   return 0;
+ }
